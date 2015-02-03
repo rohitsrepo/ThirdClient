79a5af446099cdfaceddbb18a48e29a2f28d7946
Sarthak Munshi <sarthakmunshi@gmail.com>
2015-02-04 00:00:55 +0530

    Curtain Aligned

--- reader_page/js/main.js
+++ reader_page/js/main.js
@@ -2,20 +2,9 @@ $(document).ready(function() {
 	 $(".middle a").click(function() {
         $.scrollTo($(".second-view"), { duration: 1200});
     });
-	 $(".da0").click(function() {
-        $.scrollTo($(".feed1"), { duration: 1000});
-    });
-	 $(".da1").click(function() {
-        $.scrollTo($(".feed2"), { duration: 1000});
-    });
-	 $(".da2").click(function() {
-        $.scrollTo($(".feed3"), { duration: 1000});
-    });
-	 $(".da3").click(function() {
-        $.scrollTo($(".feed4"), { duration: 1000});
-    });
     $(".info").hide();
     function toggleIcon(){
+      $(".info").toggle();
       $(".menuIcon").toggleClass("newMenuIcon")
       $(".span1").toggleClass("newSpan1");
       $(".span2").toggleClass("newSpan2");
