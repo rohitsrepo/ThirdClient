a1d9c16ae0831ef0767190ad44fde3e5f45a19f4
Sarthak Munshi <sarthakmunshi@gmail.com>
2015-02-03 19:31:55 +0530

    Image Hover Rise Implemented, Bottom Shadow Given

--- reader_page/js/main.js
+++ reader_page/js/main.js
@@ -15,4 +15,20 @@ $(document).ready(function() {
         $.scrollTo($(".feed4"), { duration: 1000});
     });
 
+     function toggleIcon(){
+  $(".menuIcon").toggleClass("newMenuIcon")
+  $(".span1").toggleClass("newSpan1");
+  $(".span2").toggleClass("newSpan2");
+  $(".span3").toggleClass("newSpan3");  
+}
+
+$(".menuIcon").click(function () {
+  toggleIcon();
+  if ($(".span1").hasClass("newSpan1")) {
+    $(".info").animate({marginLeft:0},400)
+  } else {
+    $(".info").animate({marginLeft:-220},400)
+  }
 });
+});
+
