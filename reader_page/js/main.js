dd70f8b5226d01bd6dce6c914f6439ec7f0965b2
Sarthak Munshi <sarthakmunshi@gmail.com>
2015-02-04 22:25:42 +0530

    Login Dialog Box Animated

--- reader_page/js/main.js
+++ reader_page/js/main.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 	 $(".middle a").click(function() {
         $.scrollTo($(".second-view"), { duration: 1200});
     });
+
+   //DEAL WITH CURTAIN HERE
     $(".curtain-content").hide();
     function toggleIcon(){
       $(".menuIcon").toggleClass("newMenuIcon")
@@ -20,6 +22,7 @@ $(document).ready(function() {
     }
     });
 
+    //INK EFFECT
       var $ink, x, y, ink_diameter;
   
     $('.button-ink').on('mousedown', function (e) {
@@ -55,19 +58,20 @@ $(document).ready(function() {
   $('.form-wrap').hide();
   var f=1;
   $('.sign-log').click(function() {
-    $('.form-wrap').toggle();
     f=f+1;
     if(f%2!=0){
       $('.form-wrap').removeClass('fadeInUp');
       $('.form-wrap').addClass('fadeOutDown');
-      $('.tdime').show();
-      $('.explore').show();
+      setTimeout(function() {$('.form-wrap').toggle();},300);
+      $('.tdime ,.explore').addClass('fadeInRight');
+      $('.tdime, .explore').show();
     }
     else{
-      $('section img').hide();
-      $('section .explore').hide();
+      $('.tdime').hide();
+      $('.explore').hide();
       $('.form-wrap').removeClass('fadeOutDown');
       $('.form-wrap').addClass('fadeInUp');
+      $('.form-wrap').toggle();
     }
   });
   tab = $('.tabs h3 a');
@@ -83,5 +87,33 @@ $(document).ready(function() {
 
   });
 
+  //jQuery time
+var parent, ink, d, x, y;
+$(".artwork-link").click(function(e){
+  parent = $(this).parent();
+  //create .ink element if it doesn't exist
+  if(parent.find(".ink").length == 0)
+    parent.prepend("<span class='ink'></span>");
+    
+  ink = parent.find(".ink");
+  //incase of quick double clicks stop the previous animation
+  ink.removeClass("animate");
+  
+  //set size of .ink
+  if(!ink.height() && !ink.width())
+  {
+    //use parent's width or height whichever is larger for the diameter to make a circle which can cover the entire element.
+    d = Math.max(parent.outerWidth(), parent.outerHeight());
+    ink.css({height: d, width: d});
+  }
+  
+  //get click coordinates
+  //logic = click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center;
+  x = e.pageX - parent.offset().left - ink.width()/2;
+  y = e.pageY - parent.offset().top - ink.height()/2;
+  
+  //set the position and add class .animate
+  ink.css({top: y+'px', left: x+'px'}).addClass("animate");
+})
 });
 
