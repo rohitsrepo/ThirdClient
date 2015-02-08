2e2970a293df63b3f7a9eb04bb5c7ba6233e642f
Rohit Kumar <rohitsrepo@gmail.com>
2015-02-08 08:49:57 +0530

    Add sae curtain on right

--- reader_page/js/main.js
+++ reader_page/js/main.js
@@ -1,54 +1,106 @@
 $(document).ready(function() {
-	 $(".middle a").click(function() {
-        $.scrollTo($(".second-view"), { duration: 1200});
-    });
+     $(".middle a").click(function() {
+                $.scrollTo($(".second-view"), { duration: 1200});
+        });
 
-  //DEAL WITH CURTAIN HERE
-  function addCurtain() {
-    $(".curtain-content").removeClass('fadeOutLeft');
-    $(".curtain-content").addClass('fadeInLeft');
-    $(".curtain-content").animate({marginLeft:0},400);
-    $(".curtain-feature1").addClass('fadeInUpBig');
-    setTimeout(function(){$(".curtain-feature2").addClass('fadeInUpBig')},50);
-    setTimeout(function(){$(".curtain-feature3").addClass('fadeInUpBig')},100);
-    setTimeout(function(){$(".curtain-feature4").addClass('fadeInUpBig')},150);
-    setTimeout(function(){$(".curtain-feature5").addClass('fadeInUpBig')},200);
-    setTimeout(function(){$(".curtain-feature6").addClass('fadeInUpBig')},250);
-    setTimeout(function(){$(".curtain-overlay-placeholder").addClass("curtain-overlay")},300);
-  };
+    //DEAL WITH CURTAIN HERE
 
-   function removeCurtain() {
-    $(".curtain-feature1").removeClass('fadeInUpBig');
-    $(".curtain-feature2").removeClass('fadeInUpBig');
-    $(".curtain-feature3").removeClass('fadeInUpBig');
-    $(".curtain-feature4").removeClass('fadeInUpBig');
-    $(".curtain-feature5").removeClass('fadeInUpBig');
-    $(".curtain-feature6").removeClass('fadeInUpBig');
-    $(".curtain-content").removeClass('fadeInLeft');
-    $(".curtain-content").addClass('fadeOutLeft');
-    $(".curtain-content").animate({marginLeft:-500},400);
-    $(".curtain-overlay-placeholder").removeClass("curtain-overlay");
-   };
+    var leftCurtainHandler = {
 
-   function isCurtainActive() {
-    if($(".curtain-content").hasClass("fadeInLeft")){
-      return true;
+        addCurtain: function () {
+            $(".curtain-left").removeClass('animated fadeOutLeft');
+            $(".curtain-left").addClass('animated fadeInLeft');
+            $(".curtain-left").animate({marginLeft:0}, 400);
+            $(".curtain-left .link1").addClass('animated fadeInUpBig');
+            setTimeout(function(){$(".curtain-left .link2").addClass('animated fadeInUpBig')}, 50);
+            setTimeout(function(){$(".curtain-left .link3").addClass('animated fadeInUpBig')}, 90);
+            setTimeout(function(){$(".curtain-left .link4").addClass('animated fadeInUpBig')}, 130);
+            setTimeout(function(){$(".curtain-left .link5").addClass('animated fadeInUpBig')}, 170);
+            setTimeout(function(){$(".curtain-left .link6").addClass('animated fadeInUpBig')}, 210);
+            setTimeout(function(){$(".curtain-overlay-placeholder").addClass("curtain-overlay")}, 250);
+        },
+
+         removeCurtain: function () {
+            $(".curtain-left .link1").removeClass('animated fadeInUpBig');
+            $(".curtain-left .link2").removeClass('animated fadeInUpBig');
+            $(".curtain-left .link3").removeClass('animated fadeInUpBig');
+            $(".curtain-left .link4").removeClass('animated fadeInUpBig');
+            $(".curtain-left .link5").removeClass('animated fadeInUpBig');
+            $(".curtain-left .link6").removeClass('animated fadeInUpBig');
+            $(".curtain-left").removeClass('animated fadeInLeft');
+            $(".curtain-left").addClass('animated fadeOutLeft');
+            $(".curtain-left").animate({marginLeft:-500}, 300);
+            $(".curtain-overlay-placeholder").removeClass("curtain-overlay");
+        },
+
+         isCurtainActive: function () {
+            if($(".curtain-left").hasClass("fadeInLeft")){
+                return true;
+            }
+            return false;
+        }
     }
-    return false;
-   }
+
+    var rightCurtainHandler = {
+
+        addCurtain: function () {
+            $(".curtain-right").removeClass('animated animated fadeOutRight');
+            $(".curtain-right").addClass('animated fadeInRight');
+            $(".curtain-right").animate({marginRight:0}, 400);
+            $(".curtain-right .link1").addClass('animated fadeInUpBig');
+            setTimeout(function(){$(".curtain-right .link2").addClass('animated fadeInUpBig')}, 50);
+            setTimeout(function(){$(".curtain-right .link3").addClass('animated fadeInUpBig')}, 90);
+            setTimeout(function(){$(".curtain-right .link4").addClass('animated fadeInUpBig')}, 130);
+            setTimeout(function(){$(".curtain-right .link5").addClass('animated fadeInUpBig')}, 170);
+            setTimeout(function(){$(".curtain-right .link6").addClass('animated fadeInUpBig')}, 210);
+            setTimeout(function(){$(".curtain-overlay-placeholder").addClass("curtain-overlay")}, 250);
+        },
+
+         removeCurtain: function () {
+            $(".curtain-right .link1").removeClass('animated fadeInUpBig');
+            $(".curtain-right .link2").removeClass('animated fadeInUpBig');
+            $(".curtain-right .link3").removeClass('animated fadeInUpBig');
+            $(".curtain-right .link4").removeClass('animated fadeInUpBig');
+            $(".curtain-right .link5").removeClass('animated fadeInUpBig');
+            $(".curtain-right .link6").removeClass('animated fadeInUpBig');
+            $(".curtain-right").removeClass('animated fadeInRight');
+            $(".curtain-right").animate({marginRight:-500}, 300);
+            $(".curtain-overlay-placeholder").removeClass("curtain-overlay");
+        },
+
+         isCurtainActive: function () {
+            if($(".curtain-right .link2").hasClass("fadeInUpBig")){
+                return true;
+            }
+            return false;
+        }
+    }
 
     $(".logo").click(function () {
-      if(!isCurtainActive()){
-        addCurtain();
-      }
-    else {
-        removeCurtain();
-    }
-  });
-    
+        if(!leftCurtainHandler.isCurtainActive()){
+            leftCurtainHandler.addCurtain();
+        }
+        else {
+            leftCurtainHandler.removeCurtain();
+        }
+    });
+
+    $(".auth-user").click(function () {
+        if(!rightCurtainHandler.isCurtainActive()){
+            rightCurtainHandler.addCurtain();
+        }
+        else {
+            rightCurtainHandler.removeCurtain();
+        }
+    });
+        
     $('.posts, .banner').click(function() {
-        if(isCurtainActive()){
-          removeCurtain();
+        if(leftCurtainHandler.isCurtainActive()){
+            leftCurtainHandler.removeCurtain();
+        }
+
+        if(rightCurtainHandler.isCurtainActive()){
+            rightCurtainHandler.removeCurtain();
         }
     });
 
